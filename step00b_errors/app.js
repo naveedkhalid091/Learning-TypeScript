19b3af45a93b90cdcc3a2dc853fd86581297f10b
Naveed <naveedkhalid091@yahoo.com>
2024-05-27 17:55:16 +0500

    Errors portion is done

--- step00b_errors/app.js
+++ step00b_errors/app.js
@@ -1,1 +1,9 @@
 "use strict";
+// Syntax Eror
+// lett a=10, 
+let a = 10;
+// type error
+// console.loger(a);
+// Assignability Error
+// let b="Hello Message";
+// b=10;
---
+++ step00b_errors/app.ts
@@ -0,0 +1,14 @@
+// Syntax Eror
+// lett a=10, 
+
+let a=10;
+
+// type error
+  // console.loger(a);
+
+// Assignability Error
+
+// let b="Hello Message";
+
+// b=10;
+
