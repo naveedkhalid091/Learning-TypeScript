f58137c4fb04f8fed3d5020830529535587f956b
Naveed <naveedkhalid091@yahoo.com>
2024-05-27 16:10:29 +0500

    Two Steps Done

---
+++ Step00a_json_object/app.js
@@ -0,0 +1,29 @@
+"use strict";
+// JSON string 
+const jsonString = `{
+    "name": "John", 
+    "age": 30, 
+    "isStudent": false, 
+    "skills": ["JavaScript", "Python", "Java"]
+}`;
+// JSON Object (Parsed from the JSON String)
+const jsonObject = JSON.parse(jsonString); // We can't use JSON data without Parsing it.
+console.log(jsonObject.name); // Using JSON data through dot notation.
+console.log(jsonObject["name"]); // Using JSON data through Array Method.
+// JSON Object (Defined Directly)
+const jsonObject1 = {
+    "name": "Naveed",
+    "age": 30,
+    "isStudent": true,
+    "skills": ["TypeScript", "Python", "HTML", "CSS"]
+};
+console.log(jsonObject1.name); // Using JSON data using dot notation
+console.log(jsonObject1["name"]); // Using JSON data using Array method. 
+// JavaScript Object (Defined Directly)
+const jsObject1 = {
+    name: "Zahra",
+    age: 30,
+    isStudent: true,
+    skills: ["TypeScript", "Python", "HTML", "CSS"]
+};
+console.log(jsObject1.name);
